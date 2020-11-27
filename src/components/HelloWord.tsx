dc8dce5b1c7071a7e78151ef2a1685ca3d3af450
DOU <douxianfeng@yunqishang.net>
2020-11-27 13:20:15 +0800

    chore: some

--- src/components/HelloWord.tsx
+++ src/components/HelloWord.tsx
@@ -5,13 +5,13 @@ export interface HelloWordProps {
   msg: string;
 }
 
-const [count, setCount] = useState<number>(0);
+const [count] = useState<number>(0);
 
 export const HelloWord: FC<HelloWordProps> = (props): RendererElement => {
   return (
     <div>
       <h1>{props.msg}</h1>
-      <button class="btn" onClick={() => setCount(count.value + 1)}>
+      <button class="btn" onClick={() => count.value++}>
         count is: {count.value}
       </button>
     </div>
